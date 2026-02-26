61e4478c906ddf7890baf941d23dbe877aa218f9
Zexiovo <shuliangli02@gmail.com>
2026-02-26 18:37:40 +0800

    k8s-yaml-templates

---
+++ ingress/test-canary.rb
@@ -0,0 +1,6 @@
+counts = Hash.new(0)
+100.times do
+  output = `curl -s canary.com | grep 'Canary' | awk '{print $2}' | awk -F"<" '{print $1}'`
+  counts[output.strip.split.last] += 1
+end
+puts counts
